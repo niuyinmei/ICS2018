16b8514ce246a6d4e4cf9da64b9ef3e5fcc0a342
tracer-ics2018 <tracer@njuics.org>
2018-12-21 06:45:17 -0800

    >  compile U201514655 jeffmei Linux ubuntu 4.15.0-42-generic #45~16.04.1-Ubuntu SMP Mon Nov 19 13:02:27 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  06:45:17 up 19 min,  1 user,  load average: 1.19, 0.81, 0.63 776cbb0bdbf6dd61bb93a30ba7c9a628359d686e

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -13,25 +13,28 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * cpu.gpr[1]._8[1], we will get the 'ch' register. Hint: Use `union'.
  * For more details about the register encoding scheme, see i386 manual.
  */
+#define REGSIZE 8
+#define hlSIZE 2
 
 typedef struct {
-  union{
+
+
+  /* Do NOT change the order of the GPRs' definitions. */
+
+
+	union{
 		struct{
 		   rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 		};
 		union{
 			paddr_t _32;
 			ioaddr_t _16;
-			bool _8[2];
-		} gpr[8];
+			bool _8[hlSIZE];
+		} gpr[REGSIZE];
 	};
 
-  /* Do NOT change the order of the GPRs' definitions. */
 
-  /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
-   * in PA2 able to directly access these registers.
-   */
-  union {
+	union {
 		struct {
 			uint8_t CF  :   1;
 			uint8_t DEF1:	  1;
@@ -46,8 +49,16 @@ typedef struct {
 		} eflags;
 		uint32_t flags;
 	};
-  vaddr_t eip;
 
+	bool INTR;
+
+	struct {
+		uint16_t limit;
+		uint32_t base;
+	} idtr;
+	uint16_t cs;
+
+	vaddr_t eip;
 } CPU_state;
 
 extern CPU_state cpu;
@@ -57,9 +68,9 @@ static inline int check_reg_index(int index) {
   return index;
 }
 
-#define reg_l(index) (cpu.gpr[check_reg_index(index)]._32)
-#define reg_w(index) (cpu.gpr[check_reg_index(index)]._16)
-#define reg_b(index) (cpu.gpr[check_reg_index(index) & 0x3]._8[index >> 2])
+#define reg_l(index) (cpu.gpr[check_reg_index(index)]._32) ///32位寄存器
+#define reg_w(index) (cpu.gpr[check_reg_index(index)]._16) //16位寄存器
+#define reg_b(index) (cpu.gpr[check_reg_index(index) & 0x3]._8[index >> 2])// 8位寄存器
 
 extern const char* regsl[];
 extern const char* regsw[];
