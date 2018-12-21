4381c1a456b11129d7b87002a7c761add828c99e
tracer-ics2018 <tracer@njuics.org>
2018-12-21 05:09:05 -0800

    >  compile U201514655 jeffmei Linux ubuntu 4.15.0-42-generic #45~16.04.1-Ubuntu SMP Mon Nov 19 13:02:27 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  05:09:05 up  5:08,  1 user,  load average: 0.28, 0.34, 0.81 7e55c27a1fd53938b70b1518ac6f4d0e581adaa3

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -43,31 +43,31 @@ make_EHelper(iret) {
 }
 
 make_EHelper(in) {
-  /*switch(id_dest->width)
+  switch(id_src->width)
 	{
 		case 1: t0 = pio_read_b(id_src->val); break;
 		case 2: t0 = pio_read_w(id_src->val); break;
 		case 4: t0 = pio_read_l(id_src->val); break;
 	}
   operand_write(id_dest, &t0);
-  print_asm_template2(in);*/
+  print_asm_template2(in);
 
-/*#if defined(DIFF_TEST)
+#if defined(DIFF_TEST)
   difftest_skip_ref();
-#endif*/
+#endif
 }
 
 make_EHelper(out) {
-  /*switch(id_src->width)
+  switch(id_src->width)
 	{
 		case 1: pio_write_b(id_dest->val, id_src->val); break;
 		case 2: pio_write_w(id_dest->val, id_src->val); break;
 		case 4: pio_write_l(id_dest->val, id_src->val); break;
 	}
 
-  print_asm_template2(out);*/
+  print_asm_template2(out);
 
-/*#if defined(DIFF_TEST)
+#if defined(DIFF_TEST)
   difftest_skip_ref();
-#endif*/
+#endif
 }
