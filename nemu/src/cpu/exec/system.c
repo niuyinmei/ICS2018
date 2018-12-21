c0fe60eed968218cebaea63f950461898ddcd93f
tracer-ics2018 <tracer@njuics.org>
2018-12-21 04:21:26 -0800

    >  compile U201514655 jeffmei Linux ubuntu 4.15.0-42-generic #45~16.04.1-Ubuntu SMP Mon Nov 19 13:02:27 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  04:21:26 up  4:28,  1 user,  load average: 0.46, 0.32, 0.30 6f52322b9f3784079383fb1b58e7ff4e7a8df19b

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -1,4 +1,5 @@
 #include "cpu/exec.h"
+#include "device/port-io.h"
 
 void difftest_skip_ref();
 void difftest_skip_dut();
@@ -42,7 +43,12 @@ make_EHelper(iret) {
 }
 
 make_EHelper(in) {
-  TODO();
+  switch(id_src->width)
+	{
+		case 1: t0 = pio_read_b(id_src->val); break;
+		case 2: t0 = pio_read_w(id_src->val); break;
+		case 4: t0 = pio_read_l(id_src->val); break;
+	}
 
   print_asm_template2(in);
 
@@ -52,7 +58,12 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  TODO();
+  switch(id_src->width)
+	{
+		case 1: pio_write_b(id_dest->val, id_src->val); break;
+		case 2: pio_write_w(id_dest->val, id_src->val); break;
+		case 4: pio_write_l(id_dest->val, id_src->val); break;
+	}
 
   print_asm_template2(out);
 
