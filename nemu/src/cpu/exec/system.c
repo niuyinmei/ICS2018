2d9e96b4bff3a3eb5553a97ac905935e52cf518e
tracer-ics2018 <tracer@njuics.org>
2018-12-25 06:47:18 -0800

    >  compile U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  06:47:18 up  1:59,  1 user,  load average: 0.57, 0.44, 0.34 718d52da5a14fda5968bf991dedfb7daab9f386

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -49,7 +49,13 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
-	TODO();
+	rtl_pop(&t0);
+  decoding.jmp_eip = t0;
+  decoding.is_jmp = 1;
+  rtl_pop(&t0);
+  cpu.cs = t0;
+  rtl_pop(&t0);
+  cpu.flags = t0;;
 
 	print_asm("iret");
 }
