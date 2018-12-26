4ec0d0232a7b8181f39dbbd27a6cb04131997e3d
tracer-ics2018 <tracer@njuics.org>
2018-12-26 07:53:55 -0800

    >  run U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  07:53:55 up  6:52,  1 user,  load average: 1.03, 0.75, 0.68 dbf02fe38eec84d7131a666a4a5c569f8dee5be

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -20,8 +20,7 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  /*extern void naive_uload(PCB *pcb, const char *filename);
-	naive_uload(NULL, NULL);*/
+  return;
 }
 
 _Context* schedule(_Context *prev) {
