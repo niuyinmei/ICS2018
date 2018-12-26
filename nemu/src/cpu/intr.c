8f456f0158b21148c89433b1dcf190a37eed963a
tracer-ics2018 <tracer@njuics.org>
2018-12-26 07:38:15 -0800

    >  compile U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  07:38:15 up  6:36,  1 user,  load average: 0.54, 0.69, 0.53 d5ef99eaaa60a931db0b8888df771b691db121d9

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,30 +5,31 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-  //  union {
- 	// 	GateDesc gd;
- 	// 	struct { uint32_t lo, hi; };
- 	// } item;
- 	// vaddr_t addr;
-  //
- 	// addr = 8 * NO + cpu.idtr.base;
- 	// item.lo = vaddr_read(addr, 4);
- 	// item.hi = vaddr_read(addr + 4, 4);
-  //
-  //
- 	// t1 = (item.gd.offset_15_0 & 0xFFFF)
- 	// | ((item.gd.offset_31_16 & 0xFFFF) << 16);
- 	// rtl_j(t1);
-  //
- 	// // 保存eflags cs 返回地址
- 	// rtl_push(&cpu.flags);
- 	// t0 = cpu.cs;
- 	// rtl_push(&t0);
- 	// rtl_push(&ret_addr);
-  //
- 	// cpu.eflags.IF = 0;
+
+	// TODO();
+	union {
+		GateDesc gd;
+		struct { uint32_t lo, hi; };
+	} item;
+	vaddr_t addr;
+
+	addr = 8 * NO + cpu.idtr.base;
+	item.lo = vaddr_read(addr, 4);
+	item.hi = vaddr_read(addr + 4, 4);
+
+
+	t1 = (item.gd.offset_15_0 & 0xFFFF)
+	| ((item.gd.offset_31_16 & 0xFFFF) << 16);
+	rtl_j(t1);
+
+	// 保存eflags cs 返回地址
+	rtl_push(&cpu.flags);
+	t0 = cpu.cs;
+	rtl_push(&t0);
+	rtl_push(&ret_addr);
+
+	cpu.eflags.IF = 0;
 }
 
 void dev_raise_intr() {
-  cpu.INTR = true;
 }
