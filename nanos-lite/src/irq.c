3cb55db683eb58e3c341fcd73b03d21933ba0d97
tracer-ics2018 <tracer@njuics.org>
2018-12-26 17:43:42 -0800

    >  run U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:43:42 up  1:33,  1 user,  load average: 0.94, 0.78, 0.53 575f1f5e338252754abf2fa711c3e577a4736773

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,6 +5,7 @@ extern _Context* do_syscall(_Context *c);
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
+    printf("%d\n", e.event);
     case _EVENT_YIELD: /*printf("System Trap.\n");a*/ break;
     case _EVENT_SYSCALL: do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
