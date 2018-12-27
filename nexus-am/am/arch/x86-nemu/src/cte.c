e9f995c4401700c8d419bd3f54d1983da7c31d1c
tracer-ics2018 <tracer@njuics.org>
2018-12-26 16:36:04 -0800

    >  run U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:36:04 up 25 min,  1 user,  load average: 1.08, 1.03, 0.83 616eeb27d162c5ba69f411298b3a033f10655dc

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -23,7 +23,7 @@ _Context* irq_handle(_Context *tf) {
     printf("%x\n", tf->irq);
     switch (tf->irq) {
       case 0x80: ev.event = _EVENT_SYSCALL; break;
-      case 0x81: ev.event = _EVENT_YIELD; break;
+      case 129: ev.event = _EVENT_YIELD; break;
       default: ev.event = _EVENT_ERROR; break;
     }
 
