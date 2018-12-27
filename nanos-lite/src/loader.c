283d56499825e0c445e75e120c0d1cddc2d9cbb9
tracer-ics2018 <tracer@njuics.org>
2018-12-26 17:08:08 -0800

    >  run U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:08:08 up 57 min,  1 user,  load average: 0.45, 0.36, 0.45 11f71cda1511cd774707bcd211599103b021e8fe

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,7 +10,7 @@ extern int fs_close(int fd);
 extern size_t fs_filesz(int fd);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-
+  printf("a:%s\n", filename);
   int fd = fs_open(filename, 0, 0);
 	fs_read(fd, (void *)DEFAULT_ENTRY, fs_filesz(fd));
 	fs_close(fd);
