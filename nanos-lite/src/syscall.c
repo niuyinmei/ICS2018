47af88d2bd4c31c1806bdbaabc0158a4cf3865de
tracer-ics2018 <tracer@njuics.org>
2018-12-26 19:15:42 -0800

    >  run U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  19:15:42 up  3:05,  1 user,  load average: 0.22, 0.47, 0.43 aff78a20f93e049fb63cffacdd025d91435b77ab

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,20 +3,22 @@
 #include "proc.h"
 #include "fs.h"
 
-extern void naive_uload(PCB *pcb, const char *filename);
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   uintptr_t result = -1;
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+	a[2] = c->GPR3;
+	a[3] = c->GPR4;
 
   switch (a[0]) {
     case SYS_yield:
 			result = 1;
 			break;
 		case SYS_exit:
-		 	naive_uload(NULL, "/bin/init");
-		 	break;
+      _halt((int)a[1]);
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
