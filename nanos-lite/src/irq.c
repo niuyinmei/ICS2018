00ebd88019beebd6b18b1cabbb107863dd13b579
tracer-ics2018 <tracer@njuics.org>
2018-12-26 16:39:56 -0800

    >  run U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:39:56 up 29 min,  1 user,  load average: 0.70, 0.82, 0.77 3e813c0db47c2a5697b04b9ceaae8b0e2caa8bd4

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,6 +4,7 @@
 extern _Context* do_syscall(_Context *c);
 
 static _Context* do_event(_Event e, _Context* c) {
+  printf("asd\n");
   switch (e.event) {
     switch (e.event) {
       //case _EVENT_YIELD: printf("System Trap.\n"); break;
