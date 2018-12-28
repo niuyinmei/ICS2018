bb2e98b8df829ef2a15aac8b69d11e405fc7d726
tracer-ics2018 <tracer@njuics.org>
2018-12-28 00:35:32 -0800

    >  run U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  00:35:32 up  1:12,  1 user,  load average: 0.29, 0.24, 0.25 20b1fa1d30864fc01caadfda9e735d02aa83784f

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -2,35 +2,62 @@
 #include <amdev.h>
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+	uintptr_t i = 0;
+	for(; len > 0; len--) {
+		_putc(((char*)buf)[i]);
+		i++;;
+	}
+	return i;
 }
 
 #define NAME(key) \
   [_KEY_##key] = #key,
 
 static const char *keyname[256] __attribute__((used)) = {
-  [_KEY_NONE] = "NONE",
-  _KEYS(NAME)
+	[_KEY_NONE] = "NONE",
+	_KEYS(NAME) //amdev.h里面的宏定义
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  return 0;
+	int key = read_key();
+	bool down = false;
+	//Log("key = %d\n", key);
+	if (key & 0x8000) {
+		key ^= 0x8000;
+		down = true;
+	}
+	if (key == _KEY_NONE) {
+		unsigned long t = uptime();
+		sprintf(buf, "time %u\n", t);
+	}
+	else {
+		sprintf(buf, "%s %s\n", down ? "kd" : "ku", keyname[key]);
+	}
+	return strlen(buf);
 }
 
 static char dispinfo[128] __attribute__((used));
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+	strncpy(buf, dispinfo + offset, len);
+	return len;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+	int row, col;
+	offset /= 4;
+	col = offset % screen_width();
+	row = offset / screen_width();
+
+	draw_rect((uint32_t *)buf, col, row, len/4, 1);
+	return len;
 }
 
 void init_device() {
-  Log("Initializing devices...");
-  _ioe_init();
+	Log("Initializing devices...");
+	_ioe_init();
 
-  // TODO: print the string to array `dispinfo` with the format
-  // described in the Navy-apps convention
+	// TODO: print the string to array `dispinfo` with the format
+	// described in the Navy-apps convention
+	sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", screen_width(), screen_height());
 }
