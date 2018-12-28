6a8bdfdbc2fd8d4901102ba094513b5982abdb6d
tracer-ics2018 <tracer@njuics.org>
2018-12-28 00:40:17 -0800

    >  run U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  00:40:17 up  1:17,  1 user,  load average: 0.05, 0.18, 0.23 7401c598ec12aafe9de323e33c13be0044b7edc9

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -26,55 +26,45 @@ intptr_t _syscall_(int type, intptr_t a0, intptr_t a1, intptr_t a2){
 #endif
 
 void _exit(int status) {
-  _syscall_(SYS_exit, status, 0, 0);
-  while (1);
+	_syscall_(SYS_exit, status, 0, 0);
+	while (1);
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  // _exit(SYS_open);
-  // return 0;
-  return _syscall_(SYS_open, (uintptr_t)path, flags, mode);
+	return _syscall_(SYS_open, (uintptr_t)path, flags, mode);
 }
 
 int _write(int fd, void *buf, size_t count){
-  // _exit(SYS_write);
-  // return 0;
-  return _syscall_(SYS_write, (intptr_t)fd, (intptr_t)buf, (intptr_t)count);
+ 	return _syscall_(SYS_write, fd, (uintptr_t)buf, count);
 }
 
 void *_sbrk(intptr_t increment){
-  intptr_t old_pb = program_break;
+	// panic("11111");
+	intptr_t old_pb = program_break;
 	if (_syscall_(SYS_brk, old_pb + increment, 0, 0) == 0) {
+		// panic("222");
 		program_break += increment;
 		return (void *)old_pb;
 	}
 	else {
-	   return (void *)-1;
+		  return (void *)-1;
 	}
 }
 
 int _read(int fd, void *buf, size_t count) {
-  // _exit(SYS_read);
-  // return 0;
-  return _syscall_(SYS_read, fd, (intptr_t)buf, count);
+	return _syscall_(SYS_read, fd, (uintptr_t)buf, count);
 }
 
 int _close(int fd) {
-  // _exit(SYS_close);
-  // return 0;
-  return _syscall_(SYS_close, fd, 0, 0);
+	return _syscall_(SYS_close, fd, 0, 0);
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  // _exit(SYS_lseek);
-  // return 0;
-  return _syscall_(SYS_lseek, fd, offset, whence);
+	return _syscall_(SYS_lseek, fd, offset, whence);
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
-  // _exit(SYS_execve);
-  // return 0;
-  return _syscall_(SYS_execve, (uintptr_t)fname, (uintptr_t)argv, (uintptr_t)envp);
+	return _syscall_(SYS_execve, (uintptr_t)fname, (uintptr_t)argv, (uintptr_t)envp);
 }
 
 // The code below is not used by Nanos-lite.
