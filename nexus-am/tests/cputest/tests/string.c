c7e318400efd7fc2f06e2ca16f621b1a1bbd6e93
tracer-ics2018 <tracer@njuics.org>
2018-12-31 21:56:22 -0800

    >  run U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:56:22 up 46 min,  1 user,  load average: 0.27, 0.34, 0.28 24e9cc0973275dd44e31f10a2f06696aa5afca47

--- nexus-am/tests/cputest/tests/string.c
+++ nexus-am/tests/cputest/tests/string.c
@@ -1,7 +1,7 @@
 #include "trap.h"
 
 char *s[] = {
-	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 
+	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
 	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
 	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
 	", World!\n",
@@ -20,7 +20,7 @@ int main() {
 	nemu_assert(strcmp(s[0] + 3, s[1] + 3) == -1);
 
 	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
-
+	printf("%s\n", str);
 	nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
 
 	return 0;
