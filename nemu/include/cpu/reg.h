36be15d3e9c666c5d38097d737789cc62dd60332
tracer-ics2018 <tracer@njuics.org>
2019-01-02 08:06:16 -0800

    >  compile U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  08:06:16 up  5:37,  1 user,  load average: 0.49, 0.30, 0.12 a7a71401dfce1c0b957b8b783682003af62a63c

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -2,6 +2,7 @@
 #define __REG_H__
 
 #include "common.h"
+#include "memory/mmu.h"
 
 enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
@@ -62,8 +63,8 @@ typedef struct {
 		uint32_t flags;
 	};
 
-	uint32_t cr0;
-  uint32_t cr3;
+	CR0 cr0;
+	CR3 cr3;
 	bool INTR;
 
 	struct {
