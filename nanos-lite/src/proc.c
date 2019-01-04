72acd99fadc10218db8a2262905369788c70e53f
tracer-ics2018 <tracer@njuics.org>
2019-01-04 01:07:50 -0800

    >  run U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  01:07:50 up 32 min,  1 user,  load average: 1.07, 0.58, 0.47 692dd77731b11c4149bd4aa49f7d4451ec5e7053

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -19,7 +19,7 @@ void switch_boot_pcb() {
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    Log("Hello World from Nanos-lite for the %dth time!", j);
+    //Log("Hello World from Nanos-lite for the %dth time!", j);
     j++;
     _yield();
   }
@@ -32,9 +32,9 @@ void init_proc() {
   // return;
 
   // for pa4.1 hello
-  //context_kload(&pcb[0], (void *)hello_fun);
+  context_kload(&pcb[0], (void *)hello_fun);
   //for pa4.1 PAL
-  //context_uload(&pcb[1], "/bin/init");
+  context_uload(&pcb[1], "/bin/dummy");
 
   //test
   //context_kload(&pcb[1], "/bin/init");    //reach panic
