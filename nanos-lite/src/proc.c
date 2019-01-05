b01c9eeacf7d3a18de8449165ba73f39f06bc197
tracer-ics2018 <tracer@njuics.org>
2019-01-05 06:32:08 -0800

    >  run U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  06:32:08 up 41 min,  1 user,  load average: 0.78, 0.64, 0.48 a86ff1897982c83a89e38ceddb20c7f07889fc1f

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -33,7 +33,7 @@ void init_proc() {
 
   // for pa4.1 hello kernel
   //context_kload(&pcb[0], (void *)hello_fun);
-  context_uload(&pcb[0], (void *)hello_fun);
+  context_kload(&pcb[0], (void *)hello_fun);
   //for pa4.1 PAL
   context_uload(&pcb[1], "/bin/dummy");
 
