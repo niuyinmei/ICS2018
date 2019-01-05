f1ae3926ae30811b621172bcbec9aa730a653824
tracer-ics2018 <tracer@njuics.org>
2019-01-05 07:19:24 -0800

    >  run U201514655 jeffmei Linux ubuntu 4.15.0-43-generic #46~16.04.1-Ubuntu SMP Fri Dec 7 13:31:08 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  07:19:24 up  1:28,  1 user,  load average: 0.59, 0.42, 0.57 5b22768b82963b0b1547734fe9a1f7da31cce668

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -36,7 +36,7 @@ void init_proc() {
   //context_uload(&pcb[0], "/bin/hello");
   //for pa4.1 PAL
   context_uload(&pcb[0], "/bin/init");
-  context_uload(&pcb[1], "/bin/hello");
+  context_uload(&pcb[1], (void *)hello_fun);
 
   //test
   //context_kload(&pcb[1], "/bin/init");    //reach panic
